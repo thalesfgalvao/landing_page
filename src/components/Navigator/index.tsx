95d262f5473411e7117b1d59a5617a775e405616
Thales Marques <thalesf.galvao@gmail.com>
2026-02-17 15:28:09 -0300

    Fix: change width to grids and column

--- src/components/Navigator/index.tsx
+++ src/components/Navigator/index.tsx
@@ -4,13 +4,13 @@ import * as S from './styles'
 export const Navigator = () => {
     return <S.Nav>
         <S.NavElements>
-            <S.NavElement pos="left" size='small'>LOGO</S.NavElement>
-            <S.NavElement>
+            <S.NavElement column='1 / 2' pos="left" size='small'>LOGO</S.NavElement>
+            <S.NavElement column='2 / 3'>
                 <S.MenuOption>Home</S.MenuOption>
                 <S.MenuOption>Portfólio</S.MenuOption>
                 <S.MenuOption>Sobre</S.MenuOption>
             </S.NavElement>
-            <S.NavElement pos="center" size='small'>
+            <S.NavElement column='3 / 4' pos="center" size='small'>
                 <ThemeToggle />
             </S.NavElement>
         </S.NavElements>
--- src/components/Navigator/styles.ts
+++ src/components/Navigator/styles.ts
@@ -3,45 +3,43 @@ import { styled } from '@mui/material/styles';
 interface INavElement {
   pos?: 'left' | 'center' | 'right';
   size?: 'small' | 'medium' | 'huge';
+  column?: string ;
+  row?: string ;
 }
 
 export const Nav = styled('nav')({
     display: 'flex',
+    flexDirection: 'row',
+    gridColumn: '1 / 4',
+    gridRow: '1 / 2',
 });
 
-export const NavElements = styled('div')({
-    display: 'flex',
-    flexDirection: "row",
+export const NavElements = styled('div')<INavElement> ({
+    display: 'grid',
+    gridTemplateColumns: '1fr 10fr 1fr',
     width: '100%',
-    justifyContent: 'space-between',
     gap: '2rem',
-});
+  });
 
 export const NavElement = styled('div')<INavElement>(
-  ({ pos, size }: INavElement) => ({
+  ({ pos, column, row }: INavElement) => ({
     display: 'flex',
+    alignItems: 'center',
+    width: '100%',
     flexDirection: 'row',
     padding: '0.5rem',
     gap: '5rem',
-    width: '100vw',
     backgroundColor: 'var(--surface-color)',
     boxShadow: 'var(--border-color) 0px 4px;',
+    gridColumn: column,
+    gridRow: row,
 
     justifyContent:
       pos === 'left'
         ? 'flex-start'
         : pos === 'right'
         ? 'flex-end'
-        : 'center',
-
-    maxWidth:
-    size === 'small'
-        ? '5vw'
-        : size === 'medium'
-        ? '20vw'
-        : size === 'huge'
-        ? '50vw'
-        : ""
+        : 'center'
   })
 );
 
