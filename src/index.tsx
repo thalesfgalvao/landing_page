ffc6deec943861abcf71a72360c5f91abd035a89
Thales Marques <thalesf.galvao@gmail.com>
2026-02-17 12:58:43 -0300

    Insert MainTemplate into App

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
 import './styles/theme.css';
+import './styles/global.css';
 import { MainTemplate } from './templates';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
