29b3a8eabe374717c9328c5aa3a3b23f4e4f3ae9
Thales Marques <thalesf.galvao@gmail.com>
2026-02-17 15:25:54 -0300

    Feat: create NotFoundPage

---
+++ src/Pages/NotFound/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles'
+
+export const NotFoundPage = () => {
+    return(
+        <>
+            <S.NotFoundInfo>Página não encontrada ou conteúdo vazio.</S.NotFoundInfo>
+            <S.NotFoundLabel>Page not found or empty content</S.NotFoundLabel>
+        </>
+    )
+}
