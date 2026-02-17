b88929bff236b0866d48e8fe64f8b84e20c7bc36
Thales Marques <thalesf.galvao@gmail.com>
2026-02-17 15:57:51 -0300

    Fix:adjust footer content

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,8 +3,7 @@ import * as S from './styles'
 export const Footer = () => {
     return (
         <S.footer>
-            <p>Copyright</p>
-            <p>2026</p>
+            <p>Copyright© 2026</p>
         </S.footer>
     )
 }
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,14 +1,14 @@
 import { styled } from "@mui/material/styles";
 
 export const footer = styled('footer')({
+    display: 'flex',
+    flexDirection: 'column',
     textAlign: 'center',
+    alignItems: 'center',
     justifyContent: 'center',
     gridColumn: '1/4',
     gridRow: '3',
-    display: 'flex',
-    flexDirection: 'column',
     backgroundColor: 'var(--surface-color)',
-    alignItems: 'center',
     color: 'var(--text-secondary)',
     fontWeight: '600',
     fontSize: '1.2rem',
