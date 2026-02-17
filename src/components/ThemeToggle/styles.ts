f6292da263f5dade8f783d821a2fcff3f07267c0
Thales Marques <thalesf.galvao@gmail.com>
2026-02-17 15:27:01 -0300

    Fix: change toggle theme size

--- src/components/ThemeToggle/styles.ts
+++ src/components/ThemeToggle/styles.ts
@@ -3,7 +3,10 @@ import IconButton from '@mui/material/IconButton';
 
 export const Button = styled(IconButton)(() => ({
   color: 'var(--text-primary)',
-  fontSize: '24px',
+
+  '& svg': {
+    fontSize: '3rem',
+  },
 
   '&:hover': {
     backgroundColor: 'rgba(1, 197, 250, 0.1)',
