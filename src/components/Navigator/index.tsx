3f4f815096c0d12edb17967bd492c6f3289e4f93
Thales Marques <thalesf.galvao@gmail.com>
2026-02-17 12:58:06 -0300

    Create standard nav menu

--- src/components/Navigator/index.tsx
+++ src/components/Navigator/index.tsx
@@ -1,3 +1,18 @@
+import { ThemeToggle } from '../ThemeToggle'
+import * as S from './styles'
+
 export const Navigator = () => {
-    return null
+    return <S.Nav>
+        <S.NavElements>
+            <S.NavElement pos="left" size='small'>LOGO</S.NavElement>
+            <S.NavElement>
+                <S.MenuOption>Home</S.MenuOption>
+                <S.MenuOption>Portfólio</S.MenuOption>
+                <S.MenuOption>Sobre</S.MenuOption>
+            </S.NavElement>
+            <S.NavElement pos="center" size='small'>
+                <ThemeToggle />
+            </S.NavElement>
+        </S.NavElements>
+    </S.Nav>
 }
